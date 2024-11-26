4ff5b8dea47b622856d04194f759c3bd18007bfb
alexk <alexkuleshov2005@mail.ru>
2024-11-26 17:14:14 +0500

    Добавлена первая функция

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 void f_1(){
+    ifstream filetxt("1.txt");
+    vector <string> v1;
+    string str;
+    while(getline(filetxt,str)){
+        v1.push_back(str);
+        //cout<<line<<endl;
+}
+filetxt.close();
 
 }
 
